bf7e89539bf98f9e1ad436061f3b8e3bb26e1abc
SzaboKolos <kolor1400@gmail.com>
2022-08-13 19:44:45 +0200

    Implementation: Pos

--- src/Back/Pos.java
+++ src/Back/Pos.java
@@ -1,4 +1,47 @@
 package Back;
+/**
+ * Position of Fields in a Table.
+ */
+public class Pos{
+    /**
+     * Column of the position
+     */
+    private final int c;
+    /**
+     * Row of the position
+     */
+    private final int r;
 
-public class Pos {
+    /**
+     * Returns column of the position.
+     * @return c int
+     */
+    public Integer getC(){
+        return c;
+    }
+    /**
+     * Returns row of the position.
+     * @return r int
+     */
+    public Integer getR(){
+        return r;
+    }
+
+    /**
+     * Constructs instance of Pos
+     * @param r New position's row
+     * @param c New position's column
+     */
+    public Pos(int r,int c){
+        this.r = r;
+        this.c = c;
+    }
+
+    /**
+     * Overrides built-in toString() method.
+     * @return Position in "[row][column]" format
+     */
+    public String toString(){
+        return "["+ r +"]["+ c +"];";
+    }
 }
