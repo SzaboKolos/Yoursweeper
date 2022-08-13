61c9819f4c3708ddccb5479762bf4ba1b456a250
SzaboKolos <kolor1400@gmail.com>
2022-08-13 19:46:06 +0200

    Inheritance: Mine <- Field

--- src/Back/Mine.java
+++ src/Back/Mine.java
@@ -1,4 +1,28 @@
 package Back;
+/**
+ * Descendant of Field
+ */
+public class Mine extends Field{
+    /**
+     * Constructs an instance of Mine
+     * @param p Pos, Field's position
+     */
+    public Mine(Pos p){
+        position = p;
+        icon = '☼';
+    }
 
-public class Mine {
+    /**
+     * If flagged returns the flag icon, if hidden the hidden icon,
+     * else returns the Field's icon.
+     * @return icon char
+     */
+    @Override
+    public char getIcon(){
+        if (isFlagged)
+            return flag;
+        else if (isHidden)
+            return hiddenIcon;
+        return icon;
+    }
 }
