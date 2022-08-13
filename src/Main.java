3d3341b8fb1e73042900d20d36467e38722f9ef1
SzaboKolos <kolor1400@gmail.com>
2022-08-13 19:55:00 +0200

    Implementation: Main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import Back.Game;
+import Front.GUI;
+/**
+ * @author      Kolos Szabó <szabo.kolos.01 @ gmail.com>
+ * @version     3.1
+ * @see         "https://en.wikipedia.org/wiki/Minesweeper_(video_game)"
+ */
+class Main {
+    public static void main(String[] args){
+        Game game = new Game(0);
+        new GUI(game);
+    }
+}
