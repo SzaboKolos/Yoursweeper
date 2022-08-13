46a103c34450d14a40901bc24a35ea6f4ad3cb29
SzaboKolos <kolor1400@gmail.com>
2022-08-13 19:53:28 +0200

    Implementation: GUI

--- src/Front/GUI.java
+++ src/Front/GUI.java
@@ -1,4 +1,387 @@
 package Front;
 
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.*;
+import java.awt.*;
+
+import Back.Field;
+import Back.NonMine;
+import Back.Game;
+
+/**
+ *
+ */
 public class GUI {
+    /**
+     * Attribute of the Game to be played.
+     */
+    private Game game;
+    /**
+     * The main window, contains the playfield and information fields.
+     */
+    private JFrame mainFrame;
+    /**
+     * JTable, contains the actual instance of Game, stylized.
+     */
+    private JTable playField;
+    /**
+     * Model that playField relies on.
+     */
+    private DefaultTableModel model;
+    /**
+     * Window with the game difficulty options.
+     */
+    private JFrame diffChangerFrame;
+
+    /**
+     * Creates instance of GUI.
+     * @param g Inintial game
+     */
+    public GUI(Game g){
+        game = g;
+        initFrame();
+    }
+    /**
+     * Initializes mainFrame and diffChangerFrame.
+     */
+    private void initFrame(){
+        this.mainFrame = new JFrame("Yoursweeper");
+
+        mainFrame.setSize((20*game.colNum()+10),20*game.rowNum()+30);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel tablePane = new JPanel();
+        this.playField = initTable();
+        tablePane.add(playField);
+        mainFrame.getContentPane().add(tablePane, BorderLayout.NORTH);
+        mainFrame.getContentPane().add(initMenuPanel(), BorderLayout.SOUTH);
+        mainFrame.setResizable(false);
+        mainFrame.pack();
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setVisible(true);
+
+        JPanel chooser = new JPanel();
+        chooser.add(newDiffButton(0));
+        chooser.add(newDiffButton(1));
+        chooser.add(newDiffButton(2));
+        diffChangerFrame.add(chooser);
+        diffChangerFrame.setLocationRelativeTo(mainFrame);
+    }
+
+    /**
+     *
+     * @return
+     */
+    private MouseAdapter cellRevealAdapter(){
+        return new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                int row = playField.rowAtPoint(evt.getPoint());
+                int col = playField.columnAtPoint(evt.getPoint());
+                Field thisField = game.getFieldAt(row, col);
+                if (row >= 0 && col >= 0 && row < game.rowNum() && col < game.colNum()) {
+                    if (SwingUtilities.isLeftMouseButton(evt) && !thisField.isFlagged() && thisField.isHidden()) {
+                        if (thisField.getClass().getSimpleName().equals("NonMine") && ((NonMine) thisField).getState() == game.neighborFlags(row,col))
+                            return;
+                            // revealNeighborFields(thisField);
+                        else if (thisField.getClass().getSimpleName().equals("NonMine") && ((NonMine) thisField).getState() == 0)
+                            recursiveThingy((NonMine) thisField);
+                        else if (thisField.getClass().getSimpleName().equals("Mine"))
+                            lose();
+                        else
+                            game.revealField(row, col);
+                    }
+                    else if (SwingUtilities.isRightMouseButton(evt)) {
+                        if (thisField.isFlagged()) {
+                            thisField.unflag();
+                            playField.getColumnModel().getColumn(col).setCellRenderer(new CellColorRenderer(Color.black));
+                        }
+                        else {
+                            thisField.flag();
+                            playField.getColumnModel().getColumn(col).setCellRenderer(new CellColorRenderer(Color.red));
+                        }
+                    }
+                    playField.setValueAt(thisField, row, col);
+                    System.out.println(thisField.getClass().getSimpleName()+ thisField.getPos()+" H:"+ thisField.isHidden() +"; F:"+ thisField.isFlagged());
+                }
+            }
+        };
+    }
+    private void lose(){
+        revealAllFields();
+        diffChangerFrame.setVisible(true);
+    }
+    private void revealNeighborFields(Field f) {
+        int thisR = f.getR();
+        int thisC = f.getC();
+        Game fTable = game;
+        Field[] fS = getNeighbors(thisR,thisC);
+
+        if (fTable.isNonMine(fS[0]) && fS[0].isHidden()){
+            if (((NonMine)fS[0]).getState()==0)
+                recursiveThingy((NonMine)fS[0]);
+            else {
+                fS[0].reveal();
+                playField.setValueAt(fS[0],thisR-1,thisC);
+            }
+        }
+        if (fTable.isNonMine(fS[1]) && fS[1].isHidden()) {
+            if (((NonMine)fS[1]).getState()==0)
+                recursiveThingy((NonMine)fS[1]);
+            else {
+                fS[1].reveal();
+                playField.setValueAt(fS[1],thisR,thisC+1);
+            }
+        }
+        if (fTable.isNonMine(fS[2]) && fS[2].isHidden()){
+            if (((NonMine)fS[2]).getState()==0)
+                recursiveThingy((NonMine)fS[2]);
+            else {
+                fS[2].reveal();
+                playField.setValueAt(fS[2],thisR+1,thisC);
+            }
+        }
+        if (fTable.isNonMine(fS[3]) && fS[3].isHidden()){
+            if (((NonMine)fS[3]).getState()==0)
+                recursiveThingy((NonMine)fS[3]);
+            else {
+                fS[3].reveal();
+                playField.setValueAt(fS[3],thisR,thisC-1);
+            }
+        }
+        if (fTable.isNonMine(fS[4]) && ((NonMine)fS[4]).getState()!=0 && fS[4].isHidden()){
+            fS[4].reveal();
+            playField.setValueAt(fS[4],thisR-1,thisC-1);
+        }
+        if (fTable.isNonMine(fS[5]) && ((NonMine)fS[5]).getState()!=0 && fS[5].isHidden()){
+            fS[5].reveal();
+            playField.setValueAt(fS[5],thisR-1,thisC+1);
+        }
+        if (fTable.isNonMine(fS[6]) && ((NonMine)fS[6]).getState()!=0 && fS[6].isHidden()){
+            fS[6].reveal();
+            playField.setValueAt(fS[6],thisR+1,thisC-1);
+        }
+        if (fTable.isNonMine(fS[7]) && ((NonMine)fS[7]).getState()!=0 && fS[7].isHidden()){
+            fS[7].reveal();
+            playField.setValueAt(fS[7],thisR+1,thisC+1);
+        }
+    }
+    private void revealAllFields(){
+        game.revealFields();
+        for (int r=0; r < game.rowNum();r++) {
+            for (int c=0; c < game.colNum();c++) {
+                playField.setValueAt(game.getField()[r][c].getIcon(),r,c);
+            }
+        }
+    }
+    private void recursiveThingy(NonMine field){
+        int thisR = field.getR();
+        int thisC = field.getC();
+        field.reveal();
+        playField.setValueAt(field,thisR,thisC);
+        Game fTable = game;
+
+        Field[] fS = getNeighbors(thisR,thisC);
+
+        if (fTable.isNonMine(fS[0]) && fS[0].isHidden()){
+            if (((NonMine)fS[0]).getState()==0)
+                recursiveThingy((NonMine)fS[0]);
+            else {
+                fS[0].reveal();
+                playField.setValueAt(fS[0],thisR-1,thisC);
+            }
+        }
+        if (fTable.isNonMine(fS[1]) && fS[1].isHidden()) {
+            if (((NonMine)fS[1]).getState()==0)
+                recursiveThingy((NonMine)fS[1]);
+            else {
+                fS[1].reveal();
+                playField.setValueAt(fS[1],thisR,thisC+1);
+            }
+        }
+        if (fTable.isNonMine(fS[2]) && fS[2].isHidden()){
+            if (((NonMine)fS[2]).getState()==0)
+                recursiveThingy((NonMine)fS[2]);
+            else {
+                fS[2].reveal();
+                playField.setValueAt(fS[2],thisR+1,thisC);
+            }
+        }
+        if (fTable.isNonMine(fS[3]) && fS[3].isHidden()){
+            if (((NonMine)fS[3]).getState()==0)
+                recursiveThingy((NonMine)fS[3]);
+            else {
+                fS[3].reveal();
+                playField.setValueAt(fS[3],thisR,thisC-1);
+            }
+        }
+        if (fTable.isNonMine(fS[4]) && fS[4].isHidden()){
+            if (((NonMine)fS[4]).getState()==0)
+                recursiveThingy((NonMine)fS[4]);
+            else {
+                fS[4].reveal();
+                playField.setValueAt(fS[4],thisR-1,thisC-1);
+            }
+        }
+        if (fTable.isNonMine(fS[5]) && fS[5].isHidden()){
+            if (((NonMine)fS[5]).getState()==0)
+                recursiveThingy((NonMine)fS[5]);
+            else {
+                fS[5].reveal();
+                playField.setValueAt(fS[5],thisR-1,thisC+1);
+            }
+        }
+        if (fTable.isNonMine(fS[6])&& fS[6].isHidden()){
+            if (((NonMine)fS[6]).getState()==0 )
+                recursiveThingy((NonMine)fS[6]);
+            else{
+                fS[6].reveal();
+                playField.setValueAt(fS[6],thisR+1,thisC-1);
+            }
+        }
+        if (fTable.isNonMine(fS[7]) && fS[7].isHidden()) {
+            if (((NonMine) fS[7]).getState() == 0)
+                recursiveThingy((NonMine) fS[7]);
+            else {
+                fS[7].reveal();
+                playField.setValueAt(fS[7], thisR + 1, thisC + 1);
+            }
+        }
+    }
+    private Field[] getNeighbors(int thisR, int thisC){
+        Game fTable = game;
+
+        Field SF = fTable.getFieldAt(thisR-1,thisC);
+        Field EF = fTable.getFieldAt(thisR,thisC+1);
+        Field NF = fTable.getFieldAt(thisR+1,thisC);
+        Field WF = fTable.getFieldAt(thisR,thisC-1);
+
+        Field NWF = fTable.getFieldAt(thisR-1,thisC-1);
+        Field NEF = fTable.getFieldAt(thisR-1,thisC+1);
+        Field SWF = fTable.getFieldAt(thisR+1,thisC-1);
+        Field SEF = fTable.getFieldAt(thisR+1,thisC+1);
+        return (new Field[]{SF, EF, NF, WF, NWF, NEF, SWF, SEF});
+    }
+    private JTable initTable(){
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+        centerRenderer.setVerticalAlignment( JLabel.CENTER );
+        model = makeModel();
+        JTable table = new JTable(model);
+        setTableDesign(table);
+        //table.setEnabled(false);
+        table.addMouseListener(cellRevealAdapter());
+        return table;
+    }
+    private DefaultTableModel makeModel(){
+        Object[][] fields = new Object[game.rowNum()][game.colNum()];
+        String[] colNames = new String[game.colNum()];
+        for (int k = 0; k < game.colNum(); k++){
+            colNames[k] = (k+1)+"";
+        }
+        for (int c=0;c < game.colNum(); c++){
+            for (int r=0;r < game.rowNum(); r++){
+                fields[r][c] = game.getField()[r][c];
+            }
+        }
+
+        return( new DefaultTableModel(fields,colNames) {public boolean isCellEditable(int row, int column)
+        {
+            return false;
+        }
+        });
+    }
+    private void setTableDesign(JTable table){
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+        table.setFont(new Font("Arial", Font.PLAIN, 12));
+        table.getColumnModel().setColumnSelectionAllowed(false);
+        table.getTableHeader().setReorderingAllowed(false);
+        table.setRowSelectionAllowed(false);
+        table.setBorder(BorderFactory.createLineBorder(Color.black));
+        for (int w =0;w< game.colNum(); w++) {
+            TableColumn tabCol = table.getColumnModel().getColumn(w);
+            tabCol.setPreferredWidth(20);
+            tabCol.setMinWidth(20);
+            tabCol.setMaxWidth(20);
+            tabCol.setCellRenderer( centerRenderer);
+            tabCol.setResizable(false);
+        }
+        for (int r=0; r < game.rowNum();r++) {
+            for (int c=0; c < game.colNum();c++) {
+                table.setValueAt(game.getField()[r][c],r,c);
+            }
+        }
+        table.setRowHeight(20);
+    }
+
+    /**
+     * Creates new instance of JButton with the listener to change difficulty based on diff parameter.
+     * @param diff Difficulty selector
+     * @return New JButton with the text of difficulty
+     */
+    private JButton newDiffButton(int diff){
+        String text = switch (diff) {
+            case 0 -> "Easy";
+            case 1 -> "Normal";
+            case 2 -> "Hard";
+            default -> "Custom";
+        };
+        JButton btnThis = new JButton(text);
+        btnThis.setSize(100,25);
+        btnThis.addMouseListener(new MouseAdapter() {
+            public void mouseReleased(MouseEvent e) {
+                diffChangerFrame.setVisible(false);
+                replantTable(diff);
+                mainFrame.setSize((20*game.colNum()+26),20*game.rowNum()+74);
+                mainFrame.setLocationRelativeTo(null);
+            }
+        });
+        return btnThis;
+    }
+
+
+    private JPanel initMenuPanel(){
+        diffChangerFrame = new JFrame();
+        diffChangerFrame.setSize(new Dimension(250,75));
+        diffChangerFrame.setResizable(false);
+        JButton btnNew = new JButton("New Game");
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        panel.add(btnNew);
+
+        btnNew.addMouseListener(new MouseAdapter() {
+            public void mouseReleased(MouseEvent e) {
+                diffChangerFrame.setVisible(true);
+            }
+        });
+        return panel;
+    }
+    private void replantTable(int diff){
+        game = new Game(diff);
+        model.setRowCount(game.rowNum());
+        model.setColumnCount(game.colNum());
+        setTableDesign(playField);
+        for (int r=0; r < game.rowNum();r++) {
+            for (int c=0; c < game.colNum();c++) {
+                playField.setValueAt(game.getField()[r][c],r,c);
+            }
+        }
+    }
+    static class CellColorRenderer extends DefaultTableCellRenderer {
+        Color color1;
+
+        public CellColorRenderer(Color foregroundColor) {
+            super();
+            this.color1 = foregroundColor;
+        }
+
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+            cell.setForeground(color1);
+            this.setHorizontalAlignment( JLabel.CENTER );
+            return cell;
+        }
+    }
 }
