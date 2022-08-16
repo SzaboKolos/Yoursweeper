665e8b9c11da04370c295aa9c728ffbc55564ab9
SzaboKolos <kolor1400@gmail.com>
2022-08-16 23:17:14 +0200

    Updated: Version number

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import Back.Game;
 import Front.Graphics;
 /**
  * @author      Kolos Szabó <szabo.kolos.01 @ gmail.com>
- * @version     3.1
+ * @version     4.11
  * @see         "https://en.wikipedia.org/wiki/Minesweeper_(video_game)"
  */
 class Main {
