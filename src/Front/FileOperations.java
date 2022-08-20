abe8d3dabdab8bf22485eb01ae0a813af321e21d
SzaboKolos <kolor1400@gmail.com>
2022-08-20 16:31:47 +0200

    Expanded: Saving function.

--- src/Front/FileOperations.java
+++ src/Front/FileOperations.java
@@ -2,15 +2,28 @@ package Front;
 
 import Back.Game;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class FileOperations {
 
 
-    public static void save(){
-
+    public static void save(Scores s, String filename){
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(s);
+            out.close();
+            fileOut.close();
+            System.out.println("Scores are saved in " + filename);
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
     }
-    public static ArrayList<Score> load(){
+    public static ArrayList<Score> load(String filename){
         return new ArrayList<Score>();
     }
 }
