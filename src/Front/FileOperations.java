f3ed6861e4d7d2b4e0dcdaf97d7ca542022e36b8
SzaboKolos <kolor1400@gmail.com>
2022-08-22 15:08:07 +0200

    Refactored: Local variable name "filename" in FileOperations.java

--- src/Front/FileOperations.java
+++ src/Front/FileOperations.java
@@ -1,27 +1,25 @@
 package Front;
 
-import Back.Game;
-
 import java.io.*;
 import java.util.ArrayList;
 
 public class FileOperations {
-    public static void save(Scores s, String filename){
+    public static void save(Scores s, String fileName){
         try {
             FileOutputStream fileOut =
-                    new FileOutputStream(filename);
+                    new FileOutputStream(fileName);
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             out.writeObject(s);
             out.close();
             fileOut.close();
-            System.out.println("Scores are saved in " + filename);
+            System.out.println("Scores are saved in " + fileName);
         } catch (IOException i) {
             i.printStackTrace();
         }
     }
-    public static ArrayList<Score> load(String filename){
+    public static ArrayList<Score> load(String fileName){
         ArrayList<Score> result = new ArrayList<>();
-        try (FileInputStream fis = new FileInputStream(filename);
+        try (FileInputStream fis = new FileInputStream(fileName);
              ObjectInputStream ois = new ObjectInputStream(fis)) {
             result = ((Scores)ois.readObject()).getTopList();
         } catch (IOException | ClassNotFoundException i){
